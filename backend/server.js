66032dfaa6a57db40382fb906c31222bfee4ce0b
salimsheikh <salimsheikh4u2000@gmail.com>
2024-08-17 20:43:58 +0530

    backend initialized

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from 'express'
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            "id": 1,
+            "title": "Skeleton Fight",
+            "content": "Why don't skeletons fight each other? They don't have the guts."
+        },
+        {
+            "id": 2,
+            "title": "Fake Spaghetti",
+            "content": "What do you call fake spaghetti? An impasta."
+        },
+        {
+            "id": 3,
+            "title": "Scarecrow Award",
+            "content": "Why did the scarecrow win an award? Because he was outstanding in his field!"
+        },
+        {
+            "id": 4,
+            "title": "Penguin House",
+            "content": "How does a penguin build its house? Igloos it together."
+        },
+        {
+            "id": 5,
+            "title": "Trusting Atoms",
+            "content": "Why don't scientists trust atoms? Because they make up everything!"
+        }
+    ]
+
+    res.json(jokes)
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server at http://localhost:${port}`);
+});
--- index.js
+++ index.js
@@ -3,14 +3,53 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const githubData = {
+    "login": "salimsheikh",
+    "id": 9528866,
+    "node_id": "MDQ6VXNlcjk1Mjg4NjY=",
+    "avatar_url": "https://avatars.githubusercontent.com/u/9528866?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/salimsheikh",
+    "html_url": "https://github.com/salimsheikh",
+    "followers_url": "https://api.github.com/users/salimsheikh/followers",
+    "following_url": "https://api.github.com/users/salimsheikh/following{/other_user}",
+    "gists_url": "https://api.github.com/users/salimsheikh/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/salimsheikh/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/salimsheikh/subscriptions",
+    "organizations_url": "https://api.github.com/users/salimsheikh/orgs",
+    "repos_url": "https://api.github.com/users/salimsheikh/repos",
+    "events_url": "https://api.github.com/users/salimsheikh/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/salimsheikh/received_events",
+    "type": "User",
+    "site_admin": false,
+    "name": "Salim Shaikh",
+    "company": "InfoSoft Solution",
+    "blog": "https://plugins.infosofttech.com/",
+    "location": "Mumbai",
+    "email": null,
+    "hireable": true,
+    "bio": "PHP Developer",
+    "twitter_username": "salimsheikh",
+    "public_repos": 43,
+    "public_gists": 0,
+    "followers": 0,
+    "following": 0,
+    "created_at": "2014-11-03T10:09:11Z",
+    "updated_at": "2024-08-15T10:44:37Z"
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/login', (req, res) => {
     res.send('Login Page')
-  })
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData);
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
